b36abe271606adedb2b85f08433ae4501180d0c1
Alan Languirand <alan@13protons.com>
2021-12-12 19:15:59 -0500

    init: scaffold

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import Gaveta from '@/gaveta.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Gaveta
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <gaveta />
+  </div>
+</template>
