f320368ecca2f6c652a5cc6d154b166a160ccc63
Alan Languirand <alan@13protons.com>
2021-12-12 19:34:38 -0500

    init: move in running example, try to build for ghpages

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,17 +1,183 @@
+<template>
+  <div id="app">
+    <h1>Gaveta.vue</h1>
+    <h2>A flexible drawer for your UI</h2>
+
+    <h4>
+      Meet <strong>Gaveta</strong>, the open-source and mobile-friendly drawer component bulit for Vue.js. Put anything you want in this drawer and have it animate into and out of your interface beautifully using Vue's transition framework.
+    </h4>
+
+
+    <button @click="active = true">Show Drawer</button>
+    <br/><br/>
+    <div class="row">
+      <div class="column">.column</div>
+      <div class="column">.column</div>
+    </div>
+    
+    <hr />
+    <h3>Reasons to love Gaveta:</h3>
+    <ul>
+      <li>Customizable title and close affordance</li>
+      <li>Vue2 and Vue3 compatible</li>
+      <li>Configurable with CSS variables</li>
+      <li>
+        Mobile optimized sizing, without need to target screen sizes within
+        drawer content
+      </li>
+      <li>Powered by Vue's transition framework</li>
+      <li>Use CSS 3d transforms for hardware acceleration on some devices</li>
+      <li>Works with any CSS framework</li>
+      <li>Visibility is fully controlled by parent component</li>
+    </ul>
+
+    <hr />
+    <h3>Using Gaveta</h3>
+
+<h4>Install Gaveta</h4>
+<pre><code>npm install vue-Gaveta</code></pre>
+
+<h4>Basic Usage</h4>
+
+<pre><code>&lt;template&gt;
+  &lt;div>
+    &lt;Gaveta :active="active" @close="active=false">
+      /* Info to show when the drawer is open! */
+    &lt;/Gaveta>
+  &lt;/div>
+&lt;/template&gt;
+&lt;script>
+  import Gaveta from 'vue-Gaveta';
+  export default {
+    components: { Gaveta }
+    data() { return { active: false }}
+  }
+&lt;/script>
+</code></pre>
+    
+<h4>Customize Styles</h4>
+
+<pre><code>:root {
+  /* drawer background */
+  --gaveta-background: purple;
+
+  /* main text color */
+  --gaveta-color: yellow;
+
+  /* interior padding - useful to set to zero if using container helpers */
+  --gaveta-padding: 2em;
+
+  /* color to fade out background UI - note that solid colors will not be transparent */
+  --gaveta-screen: rgba(16, 38, 56, 0.3)
+}
+</code></pre>
+
+<h4>Use Slots</h4>
+
+<pre><code>&lt;Gaveta :active="active" @close="active=false">
+  &lt;template v-slot:title>A custom title!&lt;/template>
+  &lt;template v-slot:close>Pass in anything to click on&lt;/template>
+
+  &lt;template v-slot:default>Default slot can be implied or defined explicitly&lt;/template>
+&lt;/Gaveta>
+</code></pre>
+
+<p>
+      Created by <a href="https://13protons.com">@13protons</a> for <a href="https://farebox.io">Farebox.io</a><br>
+    </p>
+
+    <Gaveta :active="active" @close="active = false">
+      <template v-slot:title>A flexible drawer for your UI</template>
+      <!-- <template v-slot:close>Done</template> -->
+
+      <p>
+        <strong>Gaveta</strong> is a fully controlled Vue component with a
+        customizable title and close button.
+      </p>
+      <p>
+        It's made to work well with your own CSS framework, has thoughtful default styles, and can be customized with CSS variables.
+      </p>
+
+      <div
+        style="width: 100%; height: 0; padding-bottom: 75%; position: relative"
+      >
+        <iframe
+          src="https://giphy.com/embed/xT5LMUT8Yy1hjMSZ5C"
+          width="100%"
+          height="100%"
+          style="position: absolute"
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <p>
+        <a
+          href="https://giphy.com/gifs/season-12-the-simpsons-12x3-xT5LMUT8Yy1hjMSZ5C"
+          >via GIPHY</a
+        >
+      </p>
+
+      <h4>This drawer is using the default styles</h4>
+
+<pre><code>:root {
+  --gaveta-background: white;
+  --gaveta-color: currentColor;
+  --gaveta-padding: 1.25em;
+  --gaveta-screen: rgba(16, 38, 56, 0.3)
+}
+</code></pre>
+    </Gaveta>
+  </div>
+</template>
+
 <script>
+import 'milligram';
+
 import Vue from 'vue';
 import Gaveta from '@/gaveta.vue';
 
 export default Vue.extend({
   name: 'ServeDev',
   components: {
-    Gaveta
-  }
+    Gaveta,
+  },
+  data() {
+    return {
+      active: false,
+    };
+  },
 });
 </script>
 
-<template>
-  <div id="app">
-    <gaveta />
-  </div>
-</template>
+<style>
+html {
+  background: rgb(34, 193, 195);
+  background: linear-gradient(
+    0deg,
+    rgba(34, 193, 195, 0.25) 0%,
+    rgba(253, 187, 45, 0.12) 100%
+  );
+
+  background-repeat: no-repeat;
+  min-height: 100vh;
+}
+hr {
+  border-color: rgba(0, 0, 0, .2);
+}
+
+h1 {
+  font-weight: bold;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  padding: 1em;
+}
+
+</style>
+
--- src/gaveta.vue
+++ src/gaveta.vue
@@ -1,78 +1,128 @@
-<script>
+<template>
+  <span>
+    <transition name="drawer" appear>
+      <div v-if="active" class="drawer is-medium">
+        <span
+          class="close-button"
+          @click="close"
+          :class="{ 'is-default': !$slots.close }"
+        >
+          <template v-if="$slots.close"><slot name="close" /></template>
+          <template v-else>
+            <svg viewBox="0 0 24 24">
+              <path
+                d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+              ></path>
+            </svg>
+          </template>
+        </span>
+        <template v-if="$slots.title">
+          <h3 class="gaveta-title"><slot name="title" /></h3>
+        </template>
 
-export default /*#__PURE__*/{
-  name: 'Gaveta', // vue component name
-  data() {
-    return {
-      counter: 5,
-      initCounter: 5,
-      message: {
-        action: null,
-        amount: null,
-      },
-    };
-  },
-  computed: {
-    changedBy() {
-      const { message } = this;
-      if (!message.action) return 'initialized';
-      return `${message.action} ${message.amount || ''}`.trim();
-    },
+        <slot name="default"></slot>
+      </div>
+    </transition>
+    <transition name="fade" appear>
+      <div v-if="active" class="screen" @click="close"></div>
+    </transition>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    active: { type: Boolean, default: false },
   },
   methods: {
-    increment(arg) {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
-      this.counter += amount;
-      this.message.action = 'incremented by';
-      this.message.amount = amount;
-    },
-    decrement(arg) {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
-      this.counter -= amount;
-      this.message.action = 'decremented by';
-      this.message.amount = amount;
-    },
-    reset() {
-      this.counter = this.initCounter;
-      this.message.action = 'reset';
-      this.message.amount = null;
+    close() {
+      this.$emit('close');
     },
   },
 };
 </script>
 
-<template>
-  <div class="gaveta">
-    <p>The counter was {{ changedBy }} to <b>{{ counter }}</b>.</p>
-    <button @click="increment">
-      Click +1
-    </button>
-    <button @click="decrement">
-      Click -1
-    </button>
-    <button @click="increment(5)">
-      Click +5
-    </button>
-    <button @click="decrement(5)">
-      Click -5
-    </button>
-    <button @click="reset">
-      Reset
-    </button>
-  </div>
-</template>
-
 <style scoped>
-  .gaveta {
-    display: block;
-    width: 400px;
-    margin: 25px auto;
-    border: 1px solid #ccc;
-    background: #eaeaea;
-    text-align: center;
-    padding: 25px;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.drawer-enter,
+.drawer-enter-from,
+.drawer-leave-to {
+  transform: translate3d(calc(85vw + 20px), 0, 0);
+}
+
+.drawer-enter-to {
+  transform: translate3d(0px, 0, 0) !important;
+}
+
+.close-button {
+  float: right;
+  padding: 0.25em;
+  cursor: pointer;
+  margin: 0.5em 0;
+}
+
+.close-button.is-default {
+  fill: var(--gaveta-background, rgba(255, 255, 255, 0.851));
+  background: var(--gaveta-color, rgba(32, 33, 36, 0.6));
+  border-radius: 50%;
+  width: 24px;
+  height: 24px;
+}
+
+.screen {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background-color: var(--gaveta-screen, rgba(16, 38, 56, 0.3));
+
+  z-index: 1999;
+}
+
+.drawer {
+  background: var(--gaveta-background, white);
+  color: var(--gaveta-color, currentColor);
+
+  box-sizing: border-box;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: 2000;
+  height: 100vh;
+
+  transition: transform 0.3s ease-in-out;
+  overflow-x: scroll;
+  padding: var(--gaveta-padding, 1.25em);
+}
+.drawer .gaveta-title {
+  padding-bottom: 0.5em;
+  border-bottom: 1px solid #888;
+}
+
+.drawer.is-medium {
+  width: 92vw;
+  box-shadow: -10px 0px 20px rgba(0, 0, 0, 0.25);
+}
+
+@media screen and (min-width: 768px) {
+  .drawer.is-medium {
+    width: 480px;
   }
-  .gaveta p {
-    margin: 0 0 1em;
+  .drawer-enter,
+  .drawer-enter-from,
+  .drawer-leave-to {
+    transform: translate3d(500px, 0, 0);
   }
+}
 </style>
